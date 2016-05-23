98e04ba5dc12e9a44251756c3b0876f83d8a3c07
wtafuck <zhaoyifeng123456@126.com>
2016-05-23 20:01:59 +0800

    Update average_degree_sampling.h

--- src/sampling/average_degree_sampling.h
+++ src/sampling/average_degree_sampling.h
@@ -1,10 +1,15 @@
 #include "storage/mgraph.h"
 #include "solver/solver.h"
+#include "cstring"
 
-class average_degree_sampling:public sae::Solver<int>{
+class average_degree_sampling//:public sae::Solver<int>
+{
 public:
-      average_degree_sampling(sae::io::MappedGraph *graph);
-      ~average_degree_sampling();
+     // average_degree_sampling(sae::io::MappedGraph *graph);
+       average_degree_sampling(std::string path);
+       ~average_degree_sampling();
       double solve(double p , double q);
       double solve(double q);
+     
+     std::string filePath;	
 };
