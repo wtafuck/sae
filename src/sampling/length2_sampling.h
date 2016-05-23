5bceec8c92316fe457816e6c744dde175c1ad5f1
wtafuck <zhaoyifeng123456@126.com>
2016-05-23 19:28:17 +0800

    Update length2_sampling.h

--- src/sampling/length2_sampling.h
+++ src/sampling/length2_sampling.h
@@ -1,13 +1,15 @@
 
 #include "storage/mgraph.h"
 #include "solver/solver.h"
+#include <cstring>
 
-class length2_sampling:public sae::Solver<int>{
+class length2_sampling//:public sae::Solver<int>
+{
 public:
-    length2_sampling(sae::io::MappedGraph *graph);
+    length2_sampling(std::string path);
     ~length2_sampling();
     double solve(double p , double q);
     double solve(double q);
+
+	std::string filePath;
 };
-
-
